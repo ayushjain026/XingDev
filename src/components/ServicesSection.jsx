c85d9a8232bc76aab3dc6a355a4d23239baccbc0
ayushjain026 <ayushjain1126@gmail.com>
2025-01-20 22:32:39 +0530

    final changes by sudhir

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -4,18 +4,19 @@ import HairKeratin from "../assets/HairKeratin.jpeg";
 
 const carddata = [
   {
-    heading: "Hair Keratin",
+    heading: "Keratin Hair Treatment",
     question: "Weak, Damaged, Unmanaged Hair ?",
     description:
-      "We got you covered ! Choose our Keratin Treatment at 50% OFF which infuse Keratin in your hair for the natural shine and outstanding glow. ",
+      "We got you covered ! Choose our Keratin Treatment at <b>50% OFF</b> which infuse Keratin in your hair for the natural shine and outstanding glow. ",
     cutoutPrice: "Rs. 9999",
     currentPrice: "Rs. 4999",
     imgUrl: "",
   },
   {
     heading: "Nanoplastia",
+    question: "Frizzy, Dull Hair, Tangled Hair messing your look?  We got you covered !",
     description:
-      "Nanoplastia is a treatment that involves applying nanoplastia protein to your hair, resulting in healthier, frizz-free, and shiny locks. The procedure takes approximately 3 hours. Choose from 5 different products.",
+      "Nanplastia is one of the most advanced Hair treatment, which regular straightening manages your hair, Nanoplastia infuses protine and brings nearly 80% of the straightening effect. <br />Choose our Nanoplastia Treatment at <b>50% OFF</b> for straightening, shiny, Gorgeous hair.",
     cutoutPrice: "Rs. 13999",
     currentPrice: "Rs. 6999",
   },
@@ -23,23 +24,31 @@ const carddata = [
 
 function ServicesSection({ setIsOpen }) {
   const isMobile = window.innerWidth <= 768; // Check if the screen width is less than or equal to 768px
-  const textStyle = {
-    fontSize: isMobile ? "16px" : "18px", // Slightly larger font size for mobile
-    color: "gray",
-    textAlign: "center", // Centered text for better mobile UX
-    lineHeight: "1", // Removes space between lines
-    margin: isMobile ? "10px" : "20px", // Adjust margins
-  };
+const containerStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexDirection: "column",
+  margin: "20px 0",
+};
 
+const textStyle = {
+  fontSize: isMobile ? "16px" : "18px",
+  color: "gray",
+  textAlign: "center",
+  lineHeight: "1.2",
+  margin: "5px 0",
+};
+  
   return (
     <section id="services-section" className="services section">
       <h2 className="services-heading">
         <span className="pri-text">“Get Shine on Your Hair”</span>
         <br />
-        <span style={textStyle}>
-          Smoother, Stronger, Shiny, Frizz-Free Healthier hair with our hair
-          treatments.
-        </span>
+        <div style={containerStyle}>
+          <h1 style={textStyle}>Smoother, Stronger, Shiny, Frizz-Free</h1>
+          <p style={textStyle}>Healthier hair with our hair treatments.</p>
+        </div>
       </h2>
       <div className="services-cards">
         {carddata.map((service, index) => (
@@ -110,13 +119,13 @@ function ServiceCard({
           {question}
         </p>
 
-        <p className="services-cards-card-content-des">{desc}</p>
+        <p className="services-cards-card-content-des" dangerouslySetInnerHTML={{ __html: desc }}></p>
         <p className="services-cards-card-content-cutoutprice">{cutoutPrice}</p>
         <p className="services-cards-card-content-currentprice">
           {actualPrice}
         </p>
         <a className="hero-cta" onClick={() => setIsOpen(true)}>
-          Book Your Appointment
+          Avail this Offer Now
         </a>
       </div>
     </div>
