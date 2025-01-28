9c9a46f9c85efeb7d093917f48e2ea82e4819056
ayushjain026 <ayushjain1126@gmail.com>
2025-01-28 21:32:48 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/1/" element={<LandingPage />} />
       </Routes>
     </Router>
   );
