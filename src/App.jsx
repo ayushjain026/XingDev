681407e15485ec2671cc9c0d04852fcc737ceac9
Ayush Jain <ayushjain1126@gmail.com>
2025-03-02 20:28:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       whatsAppRedirectionUrl: "https://api.whatsapp.com/send/?phone=8970007440&text=I+want+to+avail+hair+offer&type=phone_number&app_absent=0"
     },
     "aecs-layout": {
-      title: "Whitefiled’s",
+      title: "Whitefield's",
       email: "Xing.Aecs@gmail.com",
       location: "1st floor, Number 267, SV Plaza, <br />60 Feet Rd, AECS Layout - A Block, <br />AECS Layout, Marathahalli, Bengaluru, Karnataka 560037",
       phoneNumber: "79966-66012",
